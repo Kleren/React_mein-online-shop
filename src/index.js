d62424e84c7568df87d1407388626f59dcb20b34
Renata Klepek <klepekrenata@gmail.com>
2023-09-25 20:00:51 +0200

    BootstrapCard

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Navbar from './components/navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Product from './components/product';
 
 ReactDom.render(<Navbar />, document.getElementById('root'));
+ReactDom.render(<Product />, document.getElementById('root'));
